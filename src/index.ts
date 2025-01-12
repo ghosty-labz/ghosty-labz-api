3bbbf2760b1517664d399625e924ff8f284306d0
Gary Varner <garysarahvarner@gmail.com>
2025-01-11 20:57:08 -0500

    adding zod and resend for contact form endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { validator } from "hono/validator";
+import { z } from "zod";
+import { Resend } from "resend";
 
-const app = new Hono()
+const schema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string(),
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type Bindings = {
+  RESEND_API_KEY: string;
+};
 
-export default app
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/", (c) => {
+  return c.text("Hello from Ghosty Labz 🧪");
+});
+
+app.post(
+  "/contacts",
+  validator("json", (value, c) => {
+    const parsed = schema.safeParse(value);
+    if (!parsed.success) {
+      return c.text("Invalid!", 401);
+    }
+    return parsed.data;
+  }),
+  async (c) => {
+    const { name } = c.req.valid("json");
+    console.log(name);
+    const resend = new Resend(c.env.RESEND_API_KEY);
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["delivered@resend.dev"],
+      subject: "hello world",
+      html: "<strong>It works!</strong>",
+    });
+    return c.json(
+      {
+        message: "Created!",
+      },
+      201
+    );
+  }
+);
+
+export default app;
