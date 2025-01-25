f7a357feb94165e19ccfc9430dec432d79c20f24
Gary Varner <garysarahvarner@gmail.com>
2025-01-24 21:44:52 -0500

    switching over to aws ses as i love a challenge

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { validator } from "hono/validator";
 import { z } from "zod";
-import { Resend } from "resend";
+import type { SendEmailCommandInput } from "@aws-sdk/client-sesv2";
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
+import { render } from "@react-email/components";
 import GhostyContactEmail from "./email";
 
 const schema = z.object({
@@ -12,7 +14,8 @@ const schema = z.object({
 });
 
 type Bindings = {
-  RESEND_API_KEY: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -34,13 +37,39 @@ app.post(
   }),
   async (c) => {
     const { email } = c.req.valid("json");
-    const resend = new Resend(c.env.RESEND_API_KEY);
-    const { data, error } = await resend.emails.send({
-      from: "Ghosty Labz <labz@updates.garyvarner.me>",
-      to: email,
-      subject: "New Lead",
-      react: <GhostyContactEmail />,
+    const client = new SESv2Client({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: c.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: c.env.AWS_SECRET_ACCESS_KEY as string,
+      },
     });
+    const emailHtml = await render(<GhostyContactEmail />);
+    const params: SendEmailCommandInput = {
+      FromEmailAddress: "updates@ghostylabz.com",
+      Destination: {
+        ToAddresses: [email],
+      },
+      Content: {
+        // EmailContent
+        Simple: {
+          // Message
+          Subject: {
+            // Content
+            Data: "Hey 👋", // required
+          },
+          Body: {
+            // Body
+            Html: {
+              Data: emailHtml, // required
+              Charset: "UTF-8",
+            },
+          },
+        },
+      },
+    };
+    const command = new SendEmailCommand(params);
+    const response = await client.send(command);
     return c.json(
       {
         message: "Created!",
